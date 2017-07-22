d6ff05979e59817ccf29a31c7c2b791609dd097d
Daheng <Daheng_Zhang@outlook.com>
2017-07-22 15:36:41 +0800

    新建习惯并创建第一个项目

---
+++ css-version/sault/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var open = require('open');
+
+var targetPath = './'
+
+gulp.task('html', function() {
+  gulp.src(targetPath + '**/*.html').pipe($.connect.reload());
+});
+
+gulp.task('css', function() {
+  gulp.src(targetPath + 'style/**/*.css').pipe($.connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src(targetPath + 'script/**/*.css').pipe($.connect.reload());
+});
+
+gulp.task('build', ['html', 'css', 'js']);
+
+gulp.task('server', ['build'], function() {
+  $.connect.server({
+    root: [targetPath],
+    livereload: true,
+    port: 8080
+  });
+  open('http://localhost:8080');
+  gulp.watch(targetPath + '**/*.html', ['html']);
+  gulp.watch(targetPath + 'style/**/*.css', ['css']);
+  gulp.watch(targetPath + 'script/**/*.css', ['js']);
+});
+
+gulp.task('default', ['server']);
